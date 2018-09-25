df46d1bd655051e7f0ef6ea1b5cca02c3de2acf3
dilipkumar2k6 <dilip.kumar2k6@gmail.com>
2018-09-25 07:55:01 -0700

    feat(require): module vs script

--- printStars.js
+++ printStars.js
@@ -1,1 +1,15 @@
 
+const print = (stars, header) => {
+  console.log('*'.repeat(stars));
+  console.log(header);
+  console.log('*'.repeat(stars));
+}
+
+if (require.main === module) {
+  // running as a script
+  print(process.argv[2], process.argv[3]);
+
+} else {
+  // Being require by other file
+  module.exports = print;
+}
